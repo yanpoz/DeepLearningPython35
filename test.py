029e04a66be7c089effa9bb984abb8c69248fc41
yanpoz <yanpozdniakou1@gmail.com>
2019-11-10 01:05:03 +0300

    try

--- test.py
+++ test.py
@@ -17,19 +17,19 @@
 
 # ----------------------
 # - read the input data:
-'''
+
 import mnist_loader
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
-'''
+
 # ---------------------
 # - network.py example:
-#import network
+import network
 
-'''
+
 net = network.Network([784, 30, 10])
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
-'''
+
 
 # ----------------------
 # - network2.py example:
@@ -124,6 +124,7 @@ net.SGD(training_data, 30, 10, 0.1,
 
 
 """
+'''
 def testTheano():
     from theano import function, config, shared, sandbox
     import theano.tensor as T
@@ -149,7 +150,7 @@ def testTheano():
         print('Used the gpu')
 # Perform check:
 #testTheano()
-
+'''
 
 # ----------------------
 # - network3.py example:
@@ -157,9 +158,9 @@ import network3
 from network3 import Network, ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer # softmax plus log-likelihood cost is more common in modern image classification networks.
 
 # read data:
-training_data, validation_data, test_data = network3.load_data_shared()
+# training_data, validation_data, test_data = network3.load_data_shared()
 # mini-batch size:
-mini_batch_size = 10
+# mini_batch_size = 10
 
 # chapter 6 - shallow architecture using just a single hidden layer, containing 100 hidden neurons.
 '''
@@ -193,7 +194,7 @@ net = Network([
     SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
 net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data)
 '''
-
+'''
 # chapter 6 -  rectified linear units and some l2 regularization (lmbda=0.1) => even better accuracy
 from network3 import ReLU
 net = Network([
@@ -208,3 +209,4 @@ net = Network([
     FullyConnectedLayer(n_in=40*4*4, n_out=100, activation_fn=ReLU),
     SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
 net.SGD(training_data, 60, mini_batch_size, 0.03, validation_data, test_data, lmbda=0.1)
+'''
