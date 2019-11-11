6b523eb24e2693dde819e09bf07832a5db30b6ec
yanpoz <yanpoz@users.noreply.github.com>
2019-11-11 17:45:21 +0300

    Modified Loader

--- mnist_loader.py
+++ mnist_loader.py
@@ -16,6 +16,7 @@ import gzip
 # Third-party libraries
 import numpy as np
 
+
 def load_data():
     """Return the MNIST data as a tuple containing the training data,
     the validation data, and the test data.
@@ -38,7 +39,14 @@ def load_data():
     f = gzip.open('mnist.pkl.gz', 'rb')
     training_data, validation_data, test_data = pickle.load(f, encoding="latin1")
     f.close()
-    return (training_data, validation_data, test_data)
+    # return (training_data, validation_data, test_data)    #Old return of all data
+    n = 10
+    return (
+        tuple((training_data[0][:n], training_data[1][:n])),
+        tuple((validation_data[0][:n], validation_data[1][:n])),
+        tuple((test_data[0][:n], test_data[1][:n]))
+    )
+
 
 def load_data_wrapper():
     """Return a tuple containing ``(training_data, validation_data,
@@ -59,14 +67,22 @@ def load_data_wrapper():
     turn out to be the most convenient for use in our neural network
     code."""
     tr_d, va_d, te_d = load_data()
-    training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
+
+    # training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
+    # training_inputs = [np.matrix(img).T for img in tr_d[0]] #Better way to transpose input data
+    training_inputs = [img[np.newaxis].T for img in tr_d[0]]
     training_results = [vectorized_result(y) for y in tr_d[1]]
     training_data = zip(training_inputs, training_results)
-    validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
+
+    # validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
+    validation_inputs = [img[np.newaxis].T for img in va_d[0]]
     validation_data = zip(validation_inputs, va_d[1])
-    test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
+
+    # test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
+    test_inputs = [img[np.newaxis].T for img in te_d[0]]
     test_data = zip(test_inputs, te_d[1])
     return (training_data, validation_data, test_data)
+
 
 def vectorized_result(j):
     """Return a 10-dimensional unit vector with a 1.0 in the jth
